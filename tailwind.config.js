42eca0e82a5737d2638d3624e7718c4665ae33bb
ning.huang <ning.huang.ridson@gmail.com>
2025-03-25 15:41:06 +0800

    add Tailwind CSS configuration and integrate into global styles

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.*'],
+  
+    darkMode: ['selector', '[arco-theme="dark"]'],
+  
+    plugins: [require('@tailwindcss/typography')],
+  };
