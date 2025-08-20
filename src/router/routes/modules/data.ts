09209380fb32ed31c9843ceb01a25251dd164be0
ning.huang <ning.huang.ridson@gmail.com>
2025-08-20 15:14:23 +0800

    要素提取

--- src/router/routes/modules/data.ts
+++ src/router/routes/modules/data.ts
@@ -150,17 +150,28 @@ const DASHBOARD: AppRouteRecordRaw = {
           hideInMenu: false,
         },
       },
-    {
-      path: 'search',
-      name: 'search',
-      component: () => import('@/views/data/search/index.vue'),
-      meta: {
-        locale: '搜索',
-        requiresAuth: true,
-        roles: ['*'],
-        hideInMenu: false,
+      {
+        path: 'search',
+        name: 'search',
+        component: () => import('@/views/data/search/index.vue'),
+        meta: {
+          locale: '搜索',
+          requiresAuth: true,
+          roles: ['*'],
+          hideInMenu: false,
+        },
       },
-    },
+      {
+        path: 'entity',
+        name: 'Entity',
+        component: () => import('@/views/data/entity/index.vue'),
+        meta: {
+          locale: '要素提取',
+          requiresAuth: true,
+          roles: ['*'],
+          hideInMenu: false,
+        },
+      },
       {
         path: 'mail',
         name: 'Mail',
---
+++ src/views/data/entity/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <a-layout class="flex-layout">
+      <div class="general-card">
+        <a-tabs default-active-key="1">
+
+          <a-tab-pane key="0">
+            <template #title>
+              <icon-layers /> 实体
+            </template>
+            <!-- <EmailMsgTable /> -->
+          </a-tab-pane>
+          
+          <a-tab-pane key="1">
+            <template #title>
+              <icon-code /> 邮件
+            </template>
+            <EmailMsgTable />
+          </a-tab-pane>
+          
+          <a-tab-pane key="2">
+            <template #title>
+              <icon-at /> 邮箱
+            </template>
+            <EmailBoxTable />
+          </a-tab-pane>
+        </a-tabs>
+      </div>
+      <Footer />
+    </a-layout>
+  </template>
+  
+  <script setup lang="ts">
+  import EmailBoxTable from '@/views/data/mailbox/index.vue'
+  import EmailMsgTable from '@/views/data/mailmsg/index.vue'
+  </script>
+  
