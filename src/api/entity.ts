0c6496094c571f32140e9a20511a1646ffd23d99
ning <ning.huang.ridson@gmail.com>
2025-08-21 09:51:28 +0800

    add entity page

---
+++ src/api/entity.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import qs from 'query-string';
+
+export interface EntityReq {
+  name: string;
+  description?: string;
+  entity_type?: string;
+}
+
+export interface EntityRes extends EntityReq {
+  id: number;
+}
+
+export interface EntityParams {
+  name?: string;
+  page?: number;
+  size?: number;
+}
+
+export interface EntityListRes {
+  items: EntityRes[];
+  total: number;
+}
+
+export interface EntityDeleteParams {
+  pk: number[];
+}
+
+export function queryEntityList(params: EntityParams): Promise<EntityListRes> {
+  return axios.get('/api/v1/sys/entity', {
+    params,
+    paramsSerializer: (obj) => {
+      return qs.stringify(obj);
+    },
+  });
+}
+
+export function queryEntityAll(): Promise<EntityRes[]> {
+  return axios.get('/api/v1/sys/entity/all');
+}
+
+export function queryEntityDetail(pk: number): Promise<EntityRes> {
+  return axios.get(`/api/v1/sys/entity/${pk}`);
+}
+
+export function createEntity(data: EntityReq) {
+  return axios.post('/api/v1/sys/entity', data);
+}
+
+export function updateEntity(pk: number, data: EntityReq) {
+  return axios.put(`/api/v1/sys/entity/${pk}`, data);
+}
+
+export function deleteEntity(params: EntityDeleteParams) {
+  return axios.delete(`/api/v1/sys/entity`, {
+    params,
+    paramsSerializer: (obj) => {
+      return qs.stringify(obj);
+    },
+  });
+}
--- src/views/data/entity/index.vue
+++ src/views/data/entity/index.vue
@@ -7,17 +7,24 @@
             <template #title>
               <icon-layers /> 实体
             </template>
+            <EntityInfo />
+          </a-tab-pane>
+
+          <a-tab-pane key="1">
+            <template #title>
+              <icon-share-alt /> 关系
+            </template>
             <SPOTable />
           </a-tab-pane>
           
-          <a-tab-pane key="1">
+          <a-tab-pane key="2">
             <template #title>
               <icon-code /> 邮件
             </template>
             <EmailMsgTable />
           </a-tab-pane>
           
-          <a-tab-pane key="2">
+          <a-tab-pane key="3">
             <template #title>
               <icon-at /> 邮箱
             </template>
@@ -30,6 +37,7 @@
   </template>
   
   <script setup lang="ts">
+  import EntityInfo from './entity-info.vue';
   import EmailBoxTable from '@/views/data/mailbox/index.vue'
   import EmailMsgTable from '@/views/data/mailmsg/index.vue'
   import SPOTable from './spo.vue';
