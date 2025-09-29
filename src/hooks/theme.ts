a8bb574612845f264ee9ef261ca2663fb53b4cd9
ning <ning.huang.ridson@gmail.com>
2025-09-29 17:22:56 +0800

    fuck theme

--- src/hooks/theme.ts
+++ src/hooks/theme.ts
@@ -3,17 +3,6 @@ import { useAppStore } from '@/store';
 
 export const changeTheme = () => {
   const themeStore = useAppStore();
-  if (themeStore.theme === 'light') {
-      const cards = document.querySelectorAll('.general-card');
-      cards.forEach((card: any) => {
-        card.style.background = 'linear-gradient(146.3deg, #F0F1F7 11.56%, #D5D7E0 80%)';
-      });
-  } else {
-      const cards = document.querySelectorAll('.general-card');
-      cards.forEach((card: any) => {
-        card.style.background = '';
-      });
-  }
 }
 
 export const changeThemeColor = (color: string) => {
